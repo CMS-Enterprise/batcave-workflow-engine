85e69537766625e1a6cc3fc743e878c5f0129074
Brian Fohl <brian.fohl@gmail.com>
2024-02-08 21:01:02 -0800

    BATPT-1232 add semgrep and osemgrep commands (#7)
    * BATPT-1232 add semgrep cmd
    
    * BATPT-1232 wip
    
    ---------
    
    Co-authored-by: Brian Fohl <brian.fohl@cms.hhs.gov>

---
+++ pkg/shell/semgrep.go
@@ -0,0 +1,48 @@
+package shell
+
+import (
+	"io"
+)
+
+type semgrepCmd struct {
+	InitCmd      func() *Executable
+	experimental bool
+}
+
+// Version outputs the version of the Semgrep CLI
+// // shell: `semgrep version`
+func (s *semgrepCmd) Version() *Command {
+	if s.experimental {
+		return NewCommand(s.InitCmd().WithArgs("--help"))
+	}
+	return NewCommand(s.InitCmd().WithArgs("--version"))
+}
+
+// ScanFile runs a Semgrep scan against target artifact dir from env vars
+//
+// shell: `semgrep ci --json > ${ARTIFACT_FOLDER}/sast/semgrep-sast-report.json || true`
+func (s *semgrepCmd) ScanFile() *Command {
+	exe := s.InitCmd().WithArgs("ci", "--json")
+	if s.experimental {
+		exe = s.InitCmd().WithArgs("ci", "--json", "--experimental")
+	}
+	return NewCommand(exe)
+}
+
+// Semgrep Command with custom stdout and stderr
+func SemgrepCommand(stdout io.Writer, stderr io.Writer) *semgrepCmd {
+	return &semgrepCmd{
+		InitCmd: func() *Executable {
+			return NewExecutable("semgrep").WithOutput(stdout).WithStderr(stderr)
+		},
+	}
+}
+
+// OSemgrep Command with custom stdout and stderr
+func OSemgrepCommand(stdout io.Writer, stderr io.Writer) *semgrepCmd {
+	return &semgrepCmd{
+		InitCmd: func() *Executable {
+			return NewExecutable("osemgrep").WithOutput(stdout).WithStderr(stderr)
+		},
+	}
+}
